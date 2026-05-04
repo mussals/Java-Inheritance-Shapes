a8efce8c0cd41ea143ad43e01e201717e0144fb4
mussals <mussals@252015CD413FQH2.dunwoody.tec.mn.us>
2026-05-03 23:54:39 -0500

    Homework5-UpdatedFinal

--- src/shapePack/Rectangle.java
+++ src/shapePack/Rectangle.java
@@ -4,8 +4,7 @@ public class Rectangle extends Shape {
     public void printRectangle() {
         System.out.println("This is Rectangle. Subclass of Shape");
     }
-    
- // Method to be overridden
+
     public void calculateArea(double length, double breadth) {
         double area = length * breadth;
         System.out.println("RESULT: Area of Rectangle= " + area);
--- src/shapePack/Square.java
+++ src/shapePack/Square.java
@@ -4,11 +4,16 @@ public class Square extends Rectangle {
     public void printSquare() {
         System.out.println("This is Square. Subclass of Rectangle");
     }
-    
- // Overriding the Rectangle method
+
     @Override
-    public void calculateArea(double side, double sameSide) {
-        double area = side * side;
-        System.out.println("RESULT: Area of Square= " + area);
+    public void calculateArea(double l, double b) {
+        // If the two numbers passed are the same, it's a Square calculation
+        if (l == b) {
+            double area = l * b;
+            System.out.println("RESULT: Area of Square= " + area);
+        } else {
+            // If they are different, we use the Rectangle logic from the parent class
+            super.calculateArea(l, b);
+        }
     }
 }
